8030e35ba8b8d9e52fa4db0e3099094f91ac81a5
Agsergio04 <agsergio@g.educaand.es>
2025-04-22 10:01:00 +0200

    Parte de Sergio Acabada

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,6 @@ import org.example.datos.IUsuarioRepository
 import org.example.datos.UsuarioRepository
 import org.example.presentacion.ConsolaUI
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-// En el punto de entrada de la aplicación se configuran las implementaciones
 fun main() {
     val actividadRepo = ActividadRepository()
     val usuarioRepo = UsuarioRepository()
