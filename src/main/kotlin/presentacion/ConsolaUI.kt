3d30c916209716c6052f6dc92a4954ce508403c5
Agsergio04 <agsergio@g.educaand.es>
2025-04-23 17:22:50 +0200

    Correcion de errores V.0

--- src/main/kotlin/presentacion/ConsolaUI.kt
+++ src/main/kotlin/presentacion/ConsolaUI.kt
@@ -5,7 +5,7 @@ import org.example.aplicacion.ActividadService
 import org.example.dominio.EstadoTarea
 import org.example.dominio.Evento
 import java.util.*
-class ConsolaUI(private val servicio: ActividadService) {  
+class ConsolaUI(private val servicio: ActividadService, dashboardService: DashboardService) {
     private fun mostrarMenu() {  
     println("\n=== GESTOR DE ACTIVIDADES ===")  
     println("1. Crear nueva actividad")  
