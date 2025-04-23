5e9fa8607569f6e90b14666804fe275d7f7f9ccc
acasmor0802 <cmalfonso@iesrafaelalberti.es>
2025-04-23 13:12:14 +0200

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,21 @@
-package org.example
+package org.example  
 
-import org.example.aplicacion.ActividadService
-import org.example.datos.ActividadRepository
-import org.example.datos.IActividadRepository
-import org.example.datos.IUsuarioRepository
-import org.example.datos.UsuarioRepository
-import org.example.presentacion.ConsolaUI
+import org.example.aplicacion.ActividadService  
+import org.example.aplicacion.HistorialService  
+import org.example.datos.ActividadRepository  
+import org.example.datos.HistorialRepository  
+import org.example.datos.UsuarioRepository  
+import org.example.presentacion.ConsolaUI  
 
-fun main() {
-    val actividadRepo = ActividadRepository()
-    val usuarioRepo = UsuarioRepository()
-    val servicio = ActividadService(actividadRepo, usuarioRepo)
-
-    ConsolaUI(servicio).iniciar()
-}
+fun main() {  
+    val actividadRepo = ActividadRepository()  
+    val usuarioRepo = UsuarioRepository()  
+    val historialRepo = HistorialRepository()
+    val historialService = HistorialService(historialRepo)
+    val servicio = ActividadService(  
+        actividadRepo = actividadRepo,  
+        usuarioRepo = usuarioRepo,  
+        historialService = historialService
+    )  
+    ConsolaUI(servicio).iniciar()  
+}  
