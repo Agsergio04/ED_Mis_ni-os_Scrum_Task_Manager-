0786cc3247efc11b3aeb8b993fa05408f3d1b863
acasmor0802 <cmalfonso@iesrafaelalberti.es>
2025-04-23 13:00:22 +0200

    Create IHistorialRepository.kt

---
+++ src/main/kotlin/datos/IHistorialRepository.kt
@@ -0,0 +1,8 @@
+package org.example.datos  
+
+import org.example.dominio.Historial  
+
+interface IHistorialRepository {
+    fun agregar(historial: Historial)
+    fun obtenerPorActividad(idActividad: Int): List<Historial>
+}
