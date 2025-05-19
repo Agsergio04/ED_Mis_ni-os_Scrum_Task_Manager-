e67eff866532ec28a20d5645c31510d5ec3d249c
Agsergio04 <agsergio@g.educaand.es>
2025-05-19 23:52:34 +0200

    nada

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
 
 group = "org.example"
@@ -8,6 +9,8 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+
 
 dependencies {
     testImplementation(kotlin("test"))
